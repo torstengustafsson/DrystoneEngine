4354c7ba4ef5de0d14861793c31c4f26ec14af5e
Torsten Gustafsson <torgu529@student.liu.se>
2017-10-22 13:39:26 +0200

    added basic SDL window, and fixed cmake debugging paths for visual studio

--- include/main.cpp
+++ include/main.cpp
@@ -1,5 +1,23 @@
+#include <iostream>
+#include "SDL.h"
 
 int main(int, char**){
 
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "Failed to init SDL\n";
+		return false;
+	}
+
+	// Create our window centered at 512x512 resolution
+	SDL_Window* mainwindow = SDL_CreateWindow(
+		"My Game",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		512,
+		512,
+		SDL_WINDOW_OPENGL
+	);
+	
 	return 0;
 }
